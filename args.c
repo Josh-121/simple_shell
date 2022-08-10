d84cb31b13019abaca11a2dc3052fa129d5df9a4
Joshua Ibitoye <joshuaibitoye111@gmail.com>
2022-08-10 18:35:46 +0000

    changes

--- args.c
+++ args.c
@@ -1,36 +1,18 @@
 #include "main.h"
 char *read_line()
 {
+char *line = NULL;
+  size_t bufsize = 0; 
 
-int bufsize = 1024;
-  int position = 0;
-  char *buffer = malloc(sizeof(char) * bufsize);
-  int c;
-
-  if (!buffer) {
-    fprintf(stderr, "lsh: allocation error\n");
-    exit(EXIT_FAILURE);
-  }
-
-  while (1) {
-    c = getchar();
-
-    if (c == EOF || c == '\n') {
-      buffer[position] = '\0';
-      return buffer;
-    } else {
-      buffer[position] = c;
-    }
-    position++;
-
-    if (position >= bufsize) {
-      bufsize += 1024;
-      buffer = realloc(buffer, bufsize);
-      if (!buffer) {
-        fprintf(stderr, "lsh: allocation error\n");
-        exit(EXIT_FAILURE);
-      }
+  if (getline(&line, &bufsize, stdin) == -1){
+    if (feof(stdin)) {
+      exit(EXIT_SUCCESS); 
+    } else  {
+      perror("readline");
+      exit(EXIT_FAILURE);
     }
   }
+
+  return line;
   
 }
